ca1b1e4075a77651a293ef7e619e19418f20875e
KIM YUEN <kim_man@live.com>
2025-11-02 14:18:40 +1100

    fetching genres with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "7ec1a7b1abdd4814a60cd031280c2302",
   },
 });
