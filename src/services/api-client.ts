92f2af9ec3406c6dbacb676a2803ae900b8559e2
KIM YUEN <kim_man@live.com>
2025-11-03 13:46:52 +1100

    refactor: fetch games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key: "7ec1a7b1abdd4814a60cd031280c2302",
+    key: '7ec1a7b1abdd4814a60cd031280c2302',
   },
 });
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
