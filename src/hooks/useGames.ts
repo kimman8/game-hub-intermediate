c7e4c74a0b79d0719b58526d03be09e19e149abe
KIM YUEN <kim_man@live.com>
2025-11-08 13:33:51 +1100

    refactor: simplify time calculations with ms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { GameQuery } from '../App';
 import APIClient, { FetchResponse } from '../services/api-client';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import { Platform } from './usePlatforms';
 
 export interface Game {
@@ -30,6 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: ms('24hr'),
   });
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import APIClient, { FetchResponse } from '../services/api-client';
 import genres from '../data/genres';
 
@@ -13,8 +14,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
-    initialData: genres, 
+    staleTime: ms('24h'),
+    initialData: genres,
   });
 
 export default useGenres;
