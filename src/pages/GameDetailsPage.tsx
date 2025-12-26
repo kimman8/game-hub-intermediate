fc78a22a7c3b8ec6b8df15997e39a9a13f992796
KIM YUEN <kim_man@live.com>
2025-12-26 14:41:39 +1100

    fix the navbar

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from '@chakra-ui/react';
+import { Box, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
@@ -12,13 +12,17 @@ const GameDetailsPage = () => {
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <ScreenShots game_pk={game.game_pk} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      <Box>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </Box>
+      <Box>
+        <GameTrailer gameId={game.id} />
+        <ScreenShots game_pk={game.game_pk} />
+      </Box>
+    </SimpleGrid>
   );
 };
 
